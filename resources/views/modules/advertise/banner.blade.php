df40dab0d103396f6e2bdbd7721a256645aa570a
ym.teruya <ym.teruya@lexues.co.jp>
2017-07-20 17:35:55 +0900

    TOPページある程度OK

---
+++ resources/views/modules/advertise/banner.blade.php
@@ -0,0 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; background-color: transparent;"
+     data-ad-client="ca-pub-3972863808985760"
+     data-ad-slot="2874254337"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
---
+++ resources/views/modules/footer.blade.php
@@ -0,0 +1,37 @@
+<div class="footer-content table">
+    <div class="col-lg-2">
+        <a href="#">
+            利用規約
+        </a>
+    </div>
+    <div class="col-lg-2">
+        <a href="#">
+            ヘルプ
+        </a>
+    </div>
+    <div class="col-lg-2">
+        <a href="#">
+            問い合わせ
+        </a>
+    </div>
+    <div class="col-lg-2">
+        <a href="#">
+            ガイドライン
+        </a>
+    </div>
+    <div class="col-lg-2">
+        <a href="#">
+            プライバシーポリシー
+        </a>
+    </div>
+    <div class="col-lg-2">
+        <a href="#">
+            運営者
+        </a>
+    </div>
+</div>
+<div class="footer-copyright table">
+    <div class="col-lg-12 small">
+        <p>Copyright &copy; {{ date("Y") }} THE BLACKBOX LIST All Rights Reserved.</p>
+    </div>
+</div>
